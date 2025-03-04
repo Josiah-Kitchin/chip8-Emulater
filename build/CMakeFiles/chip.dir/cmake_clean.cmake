d707cda0c490c53375a82a4e4870150b4dbea4c4
josiah <josiahd.kitchin@gmail.com>
2025-03-04 10:41:04 -0800

    first commit

---
+++ build/CMakeFiles/chip.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chip.dir/src/main.cpp.o"
+  "CMakeFiles/chip.dir/src/main.cpp.o.d"
+  "chip"
+  "chip.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chip.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
